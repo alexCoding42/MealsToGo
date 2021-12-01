815b2ddf1eaa6e1d8c97298f182fb607cbb2ebda
alex <alexandre.cisse.ac@gmail.com>
2021-12-01 11:25:33 +0100

    chore: :rocket: Prepare the application for production deployment, fecthing live data

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,7 +4,9 @@ const liveHost = "https://us-central1-mealstogo-6e31e.cloudfunctions.net";
 const localHost = "http://localhost:5001/mealstogo-6e31e/us-central1";
 
 export const isAndroid = Platform.OS === "android";
-export const isDevelopment = process.env.NODE_ENV === "development";
+// export const isDevelopment = process.env.NODE_ENV === "development";
+export const isDevelopment = false;
 
-export const isMock = true;
+// export const isMock = true;
+export const isMock = false;
 export const host = !isDevelopment || isAndroid ? liveHost : localHost;
